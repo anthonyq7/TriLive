1d6fdef7de2b77efcbccdea69f9fc6e07ecdafb1
anthonyq7 <a.j.qin@wustl.edu>
2025-06-06 18:56:53 -0700

    Initial Commit

---
+++ TriLive/TriLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriLiveApp.swift
+//  TriLive
+//
+//  Created by Anthony Qin on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TriLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TriLiveTests/TriLiveTests.swift
@@ -0,0 +1,17 @@
+//
+//  TriLiveTests.swift
+//  TriLiveTests
+//
+//  Created by Anthony Qin on 6/6/25.
+//
+
+import Testing
+@testable import TriLive
+
+struct TriLiveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
