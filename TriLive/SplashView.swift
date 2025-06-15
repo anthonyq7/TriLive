2d73c120fa42e863a828c79216a390dc32ec270d
anthonyq7 <a.j.qin@wustl.edu>
2025-06-15 01:04:08 -0700

    Finished the searhBar structure and created a dummy list of stops.

---
+++ TriLive/SplashView.swift
@@ -0,0 +1,56 @@
+//
+//  SpashView.swift
+//  TriLive
+//
+//  Created by Anthony Qin on 6/6/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    
+    @State var isActive: Bool = false
+    
+    var body: some View {
+        
+        ZStack{
+            Color.appBackground.edgesIgnoringSafeArea(.all)
+            
+            if self.isActive {
+                HomeView()
+                    .transition(.scale)
+            } else {
+                SplashScreen()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            withAnimation(.easeInOut(duration: 0.5)){
+                                self.isActive = true
+                            }
+                        }
+                    }
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashView()
+}
+
+
+struct SplashScreen: View {
+    var body: some View {
+        ZStack{
+            Color.appBackground.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("TriLiveLogo")
+                    .resizable()
+                    .frame(width: 250, height: 250)
+                    .scaledToFit()
+                    .padding(.bottom, 125)
+                
+            }
+        }
+    }
+}
---
+++ TriLive/StopView.swift
@@ -0,0 +1,8 @@
+//
+//  StopView.swift
+//  TriLive
+//
+//  Created by Anthony Qin on 6/14/25.
+//
+
+import Foundation
