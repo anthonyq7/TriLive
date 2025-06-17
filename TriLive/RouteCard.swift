f06ddd563b991841417a680eeb70bdf85fce022d
anthonyq7 <a.j.qin@wustl.edu>
2025-06-17 02:17:46 -0700

    WIP: Added route cards and refined dummy data

---
+++ TriLive/RouteCard.swift
@@ -0,0 +1,18 @@
+//
+//  RouteCard.swift
+//  TriLive
+//
+//  Created by Anthony Qin on 6/16/25.
+//
+
+import SwiftUI
+
+struct RouteCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RouteCard()
+}
