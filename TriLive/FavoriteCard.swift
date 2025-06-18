23c431b4da50462f9bb60b5612850b90064f651a
anthonyq7 <a.j.qin@wustl.edu>
2025-06-18 00:49:16 -0700

    WIP: Favorite cards and getting up to speed

---
+++ TriLive/FavoriteCard.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteCard.swift
+//  TriLive
+//
+//  Created by Anthony Qin on 6/17/25.
+//
+
+import SwiftUI
+
+struct FavoriteCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteCard()
+}
