463151c4af178589bbe70e7905a165f8ad115a96
anthonyq7 <a.j.qin@wustl.edu>
2025-06-21 01:21:36 -0700

    Revamped RouteCard and simplified Route Structures

--- TriLive/RouteCard.swift
+++ TriLive/RouteCard.swift
@@ -7,12 +7,106 @@
 
 import SwiftUI
 
+
+/*
+ let dummyBusRoutes = [
+ BusRoute(id: 12, name: "Line 12 - Barbur/Sandy Blvd", arrivalTime: 1545, direction: "Eastbound to Sandy", realTime: 1548),
+ BusRoute(id: 75, name: "Line 75 - Chavez/Lombard", arrivalTime: 1550, direction: "Northbound to Lombard", realTime: 1551)
+ ]
+ */
+
+let dummyStop1 = Stop(
+    id: 258,
+    name: "Hawthorne & 12th",
+    routeList: dummyRoutes1
+)
+
 struct RouteCard: View {
+    let parentStop: Stop
+    let line: Route
+    @State var isFavorite: Bool = false
+    @State var isSelected: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            HStack {
+                Text(line.name)
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                Text(timeConverter(time: line.realTime).description)
+                    .padding(.horizontal)
+                    .foregroundStyle(.tint) //eventually make this time into currentTime - realTime so that it displays the minutes remaining but this shall suffice for now
+                
+                Spacer()
+                //Create Favorite Button
+                if !isFavorite {
+                    Image(systemName: "star")
+                        .font(.title2)
+                        .padding(.horizontal)
+                        .onTapGesture {
+                            isFavorite.toggle()
+                        }
+                } else {
+                    Image(systemName: "star.fill")
+                        .foregroundStyle(.yellow)
+                        .font(.title2)
+                        .padding(.horizontal)
+                        .onTapGesture {
+                            isFavorite.toggle()
+                        }
+                }
+            }
+            
+            Text(line.direction)
+                .padding(.vertical, 5)
+                .fontWeight(.bold)
+                .lineLimit(1)
+        }
+        .lineLimit(1)
+        .foregroundStyle(.white)
+        .padding(.vertical, 20)
+        .background(Color.appBackground)
+        .overlay(
+            Rectangle()
+                .stroke(isSelected ? Color.green: Color.white, lineWidth: 2.5) //Convert to green when selected
+        )
+        .onTapGesture {
+            isSelected.toggle()
+        }
     }
 }
 
+
 #Preview {
-    RouteCard()
+    RouteCard(parentStop: dummyStop1, line: dummyRoutes1[2])
 }
+
+func timeConverter(time: Int) -> String { //Converts time from hrmin to hr:min
+    
+    var hour: Int {
+        let temp = time/100
+        
+        if temp > 12{
+            return temp - 12
+        } else {
+            return temp
+        }
+        
+    }
+    
+    var minute: String {
+        
+        if time%100 < 10 {
+            return "0" + String(time%100)
+        } else {
+            return String(time%100)
+        }
+        
+    }
+    
+    return String(hour) + ":" + String(minute)
+}
+
--- TriLive/SettingsView.swift
+++ TriLive/SettingsView.swift
@@ -9,10 +9,34 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.appBackground.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                
+                HStack {
+                    Text("Settings")
+                        .foregroundStyle(Color.white)
+                        .font(.system(size: 48, weight: .medium, design: .default))
+                        .padding(.leading, 16)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.75)
+                    
+                    Spacer()
+                    
+                }
+                .padding()
+            } //VStack
+        } // ZStack
     }
 }
 
 #Preview {
     SettingsView()
+}
+
+struct SettingsCard: View {
+    var body: some View {
+        
+    }
 }
