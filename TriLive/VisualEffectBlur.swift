9c98f4dd9e4f1a4e94c9803566eca16681df3a3a
Brian Maina <brianmaina.mach@gmail.com>
2025-06-23 13:03:55 +0300

    Kinda messed up the merge and got rid of the work i did but i had it in the word doc

---
+++ TriLive/VisualEffectBlur.swift
@@ -0,0 +1,19 @@
+//
+//  VisualEffectBlur.swift
+//  TriLive
+//
+//  Created by Brian Maina on 6/23/25.
+//
+
+import SwiftUI
+import UIKit
+
+struct VisualEffectBlur: UIViewRepresentable {
+  let blurStyle: UIBlurEffect.Style
+
+  func makeUIView(context: Context) -> UIVisualEffectView {
+    UIVisualEffectView(effect: UIBlurEffect(style: blurStyle))
+  }
+  func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+  }
+}
