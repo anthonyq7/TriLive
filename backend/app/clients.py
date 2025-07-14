9f0ab70c25d970e8894637b0e3481140a9b325d1
Brian Maina <brianmaina.mach@gmail.com>
2025-07-14 14:16:32 +0300

    fixing redis

--- backend/app/clients.py
+++ backend/app/clients.py
@@ -1,8 +1,7 @@
 # app/clients.py
 import redis, os
 
-redis_client = redis.Redis(
-    host=os.getenv("REDIS_HOST", "localhost"),
-    port=int(os.getenv("REDIS_PORT", 6379)),
+redis_client = redis.from_url(
+    os.getenv("REDIS_URL", "redis://localhost:6379/0"),
     db=0,
 )
