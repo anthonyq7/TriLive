6cd9fd9b2b1649cf89e2291f79689759676dc011
Brian Maina <brianmaina.mach@gmail.com>
2025-07-17 16:17:05 +0300

    fixed stop schema

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,16 +8,16 @@ load_dotenv()
 DATABASE_URL=os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
+
 SessionLocal=sessionmaker(bind=engine)
 
 class Stop(Base):
     __tablename__ = "StopTable"
-    id = Column(Integer, primary_key=True)
+    trimet_id = Column(Integer, primary_key=True)
     name = Column(String,nullable=False)
-    lat = Column(Float, nullable=False)
-    lon = Column(Float, nullable=False)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
     dir = Column(String, nullable=False)
-    trimet_id = Column(Integer, unique=True, nullable=True)
 
 class Favorite(Base):
     __tablename__ = "Favorites"
